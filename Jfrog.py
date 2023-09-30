0b5d870a0eb1a912304c151cf5b58d1b4c731713
Hemanth Kumar <dkrhemanth@gmail.com>
2023-10-01 05:02:46 +0530

    Update Jfrog.py

--- Jfrog.py
+++ Jfrog.py
@@ -1,1 +1,19 @@
+import requests
 
+url = "http://192.168.0.104:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+file_path = '/home/osboxes/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+username = 'admin'
+password = 'Admin@123'
+
+with open(file_path, 'rb') as file:
+   response = requests.put(url, auth=(username, password), data=file)
+
+if response.status_code == 201:
+    print("\nPUT request was successful")
+else:
+   print(f"PUT request failed with status code {response.status_code}")
+   print("Response content:")
+   print(response.text)  
+
+#response = requests.put(url + "/data/" + file_name, auth=auth, data=open(file_name, "rb"))
+#print(response.status_code)
